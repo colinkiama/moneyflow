e414f71fcf7111def14faa0dfcfbaf2547e2b7e0
Colin Kiama <colinkiama@gmail.com>
2022-10-26 18:50:31 +0100

    Add text variants to heading in account card

--- components/AccountCard.tsx
+++ components/AccountCard.tsx
@@ -2,15 +2,15 @@ import React from 'react';
 import {View} from 'react-native';
 import {Card, Text} from 'react-native-paper';
 
-const AccountCard = (props)_ => {
+const AccountCard = (props)_=> {
   return (
     <Card>
       <Card.Title title={props.title} />
       <Card.Content>
         <View>
           {/* Turn this into it's own component */}
-          <Text>{heading.title}</Text>
-          <Text>{heading.label}</Text>
+          <Text variant="titleLarge">{props.heading.title}</Text>
+          <Text variant="labelSmall">{props.heading.label}</Text>
         </View>
       </Card.Content>
     </Card>
